3b49803d6ced744696c104b3895a84b47fbb56ec
LightFex77 <max706genesoviedo@gmail.com>
2023-08-22 14:59:36 -0400

    Cree el componente Home

---
+++ frontend/src/complemetos/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home-container">
+            <section className="welcome-section"></section>
+            <section className="account-section"></section>
+        </div>
+    )
+}
+
+export default Home

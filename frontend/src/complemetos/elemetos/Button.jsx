a2d740eb1c9af8e8e12a6fbbce88ac3fffd3f7b8
LightFex77 <max706genesoviedo@gmail.com>
2023-08-22 15:10:31 -0400

    Termine la construccion del elemento Button

--- frontend/src/complemetos/elemetos/Button.jsx
+++ frontend/src/complemetos/elemetos/Button.jsx
@@ -1,8 +1,14 @@
-const Button = () => {
-    return(
-        <>
-        </>
-    )
-}
+const Button = (onClick, contentButton, extraClass, labelContent) => {
+  return (
+    <div className="button-container-elm">
+      <label form="button-elm-id">
+        {labelContent}
+        <button id="button-elm-id" className={`button-elm ${extraClass}`} onClick={onClick}>
+          {contentButton}
+        </button>
+      </label>
+    </div>
+  );
+};
 
 export default Button;

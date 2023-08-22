6a9ef4a22cc510dbca1e50f374a95b0a5632e261
LightFex77 <max706genesoviedo@gmail.com>
2023-08-22 17:14:59 -0400

    Termine de realizar los estilos del apartado de register

--- frontend/src/complemetos/Register.jsx
+++ frontend/src/complemetos/Register.jsx
@@ -4,14 +4,13 @@ import Button from "./elemetos/Button";
 const Register = () => {
   return (
     <div className="home-container">
-        <section className=""><h1>Rellena los datos:</h1></section>
+        <section className="title-register-s"><h1>Rellena los datos:</h1></section>
         <section className="register-info">
             <Input placeholder="Nombre..." labelContent="Username: "/>
             <Input placeholder="Contraseña..." labelContent="Contraseña: " typeText="password"/>
             <Input placeholder="Email.." labelContent="Email: "/>
             <Button contentButton="Registrarse"/>
         </section>
-
     </div>
   );
 };

f79df917043b6fa01d98ea38ce22b0539eae4a34
LightFex77 <max706genesoviedo@gmail.com>
2023-08-22 15:13:31 -0400

    Termine la construccion del elemento Input

--- frontend/src/complemetos/elemetos/Input.jsx
+++ frontend/src/complemetos/elemetos/Input.jsx
@@ -1,8 +1,18 @@
-const Input = () => {
-    return(
-        <>
-        </>
-    )
-}
+const Input = (labelContent, extraClass, onChange, placeholder, value) => {
+  return (
+    <div className="input-container-elm">
+      <label form="input-elm-id">
+        {labelContent}
+        <input
+          id="input-elm-id"
+          className={`input-elm ${extraClass}`}
+          onChange={onChange}
+          placeholder={placeholder}
+          value={value}
+        ></input>
+      </label>
+    </div>
+  );
+};
 
 export default Input;

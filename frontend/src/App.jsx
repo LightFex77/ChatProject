3f6fb82f8f8a149ac396ba21e93999f8998a4acc
LightFex77 <max706genesoviedo@gmail.com>
2023-08-22 16:15:54 -0400

    Cree el componente de Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './styles/interfaceLogin.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormSumbit from "./complemetos/FormSumbit";
 import Home from "./complemetos/Home";
+import Register from './complemetos/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/chat-room" element={<div className="room-container"><FormSumbit /></div>}/>
         <Route path="/" element={<div className='interface-container-g'><Home /></div>}/>
+        <Route path='/register-account' element={<div className='interface-container-g'><Register /></div>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/complemetos/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return(
+        <div className="home-container">
+        
+        </div>
+    )
+}
+
+export default Register

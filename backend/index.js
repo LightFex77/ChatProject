e210bcde011310e047f928a20a408af176613d58
LightFex77 <max706genesoviedo@gmail.com>
2023-08-25 20:27:13 -0400

    Agregue la funcion del like, tambien la hora

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ const { Server } = require("socket.io");
 const loginRouter = require("./routers/login.router");
 const { AppDataSource } = require("./utils/connection");
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -18,18 +17,28 @@ const io = new Server(server, {
   },
 });
 
+const messages = [];
+
 io.on("connection", (socket) => {
   console.log("Usuario conectado");
 
-  // Manejar mensajes del cliente
   socket.on("message", (message) => {
-    console.log(`Mensaje recibido: ${message}`);
-
-    // Enviar el mensaje a todos los clientes conectados
+    messages.push(message);
     io.emit("message", message);
   });
 
-  // Manejar desconexiones de clientes
+  socket.on("likes", ({ messageId, userId }) => {
+    const messageToUpdate = messages.find((msg) => msg.messageId === messageId);
+    if (messageToUpdate) {
+      if (messageToUpdate.likes.includes(userId)) {
+        messageToUpdate.likes = messageToUpdate.likes.filter((id) => id !== userId);
+      } else {
+        messageToUpdate.likes.push(userId);
+      }
+      io.emit("likes", messageToUpdate);
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log("Usuario desconectado");
   });
@@ -39,4 +48,5 @@ server.listen(3000, async () => {
   console.log("Servidor escuchando en el puerto 3000");
   await AppDataSource.initialize();
   console.log("Conectado a la base de datos");
+
 });
--- frontend/src/complemetos/FormSumbit.jsx
+++ frontend/src/complemetos/FormSumbit.jsx
@@ -1,46 +1,73 @@
-import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import { useRedirectToHome } from '../hooks/useRedirect';
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { useRedirectToHome } from "../hooks/useRedirect";
 
 const FormSubmit = () => {
   useRedirectToHome();
   const [socket, setSocket] = useState(null);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [idUser, setIdUser] = useState(null);
 
-  // TODO: llevar a un hook
   useEffect(() => {
     // Conectar al servidor de Socket.io
-    const socket = io('http://localhost:3000');
+    const socket = io("http://localhost:3000");
     setSocket(socket);
 
-    // Obtener el ID de socket asignado por el servidor
-    socket.on('connect', () => {
-      const userId = localStorage.getItem('userMine');
+    socket.on("connect", () => {
+      const userId = localStorage.getItem("userMine");
       const userObj = JSON.parse(userId);
-      setUserId(userObj.user.username);
+      setUserName(userObj.user.username);
+      setIdUser(userObj.user.id);
     });
 
-    // Escuchar mensajes del servidor
-    socket.on('message', (data) => {
-      setMessages((prevMessages) => [...prevMessages, data]);
+    socket.on("message", (data) => {
+      if (!messages.some((msg) => msg.messageId === data.messageId)) {
+        setMessages((prevMessages) => [...prevMessages, data]);
+      }
     });
 
-    // Limpiar al desmontar el componente
+    socket.on("likes", (updatedMessage) => {
+      // Actualizar el mensaje en el estado local
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg.messageId === updatedMessage.messageId ? updatedMessage : msg
+        )
+      );
+    });
+
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [messages]);
 
-  const handleSubmit = (e) => {
+  const generateMessageId = () => {
+    return Math.random().toString(36).substring(2, 15);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const msjId = generateMessageId();
+    const hora = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
     // Enviar el mensaje al servidor con el ID de usuario
-    socket.emit('message', { body: message, userId });
+    await socket.emit("message", {
+      body: message,
+      userId: userName,
+      messageId: msjId,
+      creationTime: hora,
+      likes: [],
+    });
 
     // Limpiar el campo de entrada
-    setMessage('');
+    setMessage("");
+  };
+
+  const handleLikes = async (messageId) => {
+    // Emitir el evento "likes" al servidor con el ID del mensaje y el ID del usuario
+    socket.emit("likes", { messageId: messageId, userId: idUser });
   };
 
   return (
@@ -51,28 +78,34 @@ const FormSubmit = () => {
             <li
               key={index}
               className={`li-message li-message-${
-                msg.userId === userId ? 'right' : 'left'
-              }`}>
+                msg.userId === userName ? "right" : "left"
+              }`}
+            >
               <span
                 className="initial-name"
                 style={
-                  msg.userId === userId
-                    ? { background: '#4F8136' }
-                    : { background: '#48494D' }
-                }>
+                  msg.userId === userName
+                    ? { background: "#4F8136" }
+                    : { background: "#48494D" }
+                }
+              >
                 {msg.userId.charAt(0)}
               </span>
               <div className="li-message-contenido">
+                <p><span>{`[${msg.creationTime}]`}</span> {msg.userId}</p>
                 <span className="span-message">{msg.body}</span>
-                {msg.userId !== userId ? (
-                  <button className="like-message-button">
+                {msg.userId !== userName ? (
+                  <button
+                    className="like-message-button"
+                    onClick={() => handleLikes(msg.messageId)}
+                  >
                     <svg id="Layer_1" viewBox="0 0 60 60">
                       <polygon points="29.85 24.27 16.15 10.58 3.08 23.66 16.77 37.35 29.85 50.43 29.85 50.43 42.93 37.35 42.93 37.35 56.63 23.65 43.55 10.57 29.85 24.27" />
                     </svg>
-                    <span className="likes-count">{msg.likes ?? 0}</span>
+                    <span className="likes-count">{msg.likes.length ?? 0}</span>
                   </button>
                 ) : (
-                  <span className="likes-count">{msg.likes ?? 0}</span>
+                  <span className="likes-count">{msg.likes.length ?? 0}</span>
                 )}
               </div>
             </li>
